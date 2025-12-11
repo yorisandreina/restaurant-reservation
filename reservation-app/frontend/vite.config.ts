09fbfe679a50014cef339b7cd1d16842fafd0e47
Andreina <yorisandreina2003@gmail.com>
2025-12-11 15:20:40 +0100

    update vite config to include env variable

--- reservation-app/frontend/vite.config.ts
+++ reservation-app/frontend/vite.config.ts
@@ -1,42 +1,47 @@
-import { defineConfig } from 'vite'
-import { VitePWA } from "vite-plugin-pwa"
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
-import path from "path"
+import { defineConfig, loadEnv } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      manifest: {
-        name: "My Booking App",
-        short_name: "Booking",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        icons: [
-          { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://45ce63e855cf.ngrok-free.app",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest: {
+          name: "My Booking App",
+          short_name: "Booking",
+          start_url: "/",
+          display: "standalone",
+          background_color: "#ffffff",
+          icons: [
+            { src: "/pwa-192.png", sizes: "192x192", type: "image/png" },
+            { src: "/pwa-512.png", sizes: "512x512", type: "image/png" },
+          ],
+        },
+      }),
+    ],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
